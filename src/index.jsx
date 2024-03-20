156a759b07a657ace7e52400a70e811663ca9a42
Ronpu <ronpu@hotmail.fr>
2024-03-20 12:45:41 +0100

    Ajout début de pages et de la navigation

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,27 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom/client";
+// import {
+// 	createBrowserRouter,
+// 	RouterProvider,
+// } from "react-router-dom";
+
+// import Place from "./pages/Place";
+
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		element: <Place />,
+// 	},
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+// 	<React.StrictMode>
+// 		<RouterProvider router={router} />
+// 	</React.StrictMode>
+// );
+
+
+
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
@@ -5,13 +29,31 @@ import {
 	RouterProvider,
 } from "react-router-dom";
 
+import Home from "./pages/Home"; // TEMPORAIRE (Normalement, l'application s'ouvre sur la map ou la connexion si pas déjà connecté )
 import Place from "./pages/Place";
+import Signin from "./pages/Signin";
+import Login from "./pages/Login";
+// import Error from "./pages/Error";
+
 
 const router = createBrowserRouter([
 	{
 		path: "/",
+		element: <Home />,	// TEMPORAIRE (normalement Place)
+	},
+	{
+		path: "/place",
 		element: <Place />,
 	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/signin",
+		element: <Signin />,
+	},
+	
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/Home.css";
+
+function navigateTo(path){
+    console.log("AAAA???")
+    let navigate = new useNavigate()    
+	navigate(path)
+}
+
+function Home()
+{
+    const navigate = new useNavigate() 
+	return (
+		<div id="body">
+
+			<table class="menuNavigate">
+				<tr>
+					<td><button onClick={()=>{navigate('/Place')}}>Place</button></td>
+					<td><button onClick={()=>{navigate('/Login')}}>Login</button></td>
+					<td><button onClick={()=>{navigate('/Signin')}}>Signin</button></td>
+				</tr>
+			</table>
+
+		</div>
+	);
+};
+
+export default Home;
