1601711c47b3dd4bc5259cdb67c81ef2f1837b95
Ronpu <ronpu@hotmail.fr>
2024-05-22 17:11:35 +0200

    Menu parametre global mis en place

--- src/pages/GlobalPage.js
+++ src/pages/GlobalPage.js
@@ -3,6 +3,8 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import "../css/GlobalPage.css";
 
 // IMPORT DES IMAGES :
+import wiveLogoTitre from "../assets/whiteMode/wiveLogoTitre.svg";
+
 import homeWhiteMode from "../assets/whiteMode/homeWhiteMode.svg";
 import homeWhiteModeChecked from "../assets/whiteMode/homeWhiteModeChecked.svg";
 import plusWhiteMode from "../assets/whiteMode/plusWhiteMode.svg";
@@ -28,10 +30,30 @@ function AddCamera()
 	return (
 		<div id="body">
 
-            <iframe id='iframeShownPage' src={urlOrigin + '/' + paramPage} frameBorder="0">
+            {/* Icone principale + icone de paramêtre (quasiment) */}
+            <div className="iconContainer">
+                <img src={wiveLogoTitre} /> 
+				<div id="param" className="paramIcon" onClick={(event)=>toggleMenuParam()}><div className="bar1"></div><div className="bar2"></div><div className="bar3"></div></div>
+			</div>
 
-            </iframe>
+            {/* Menu de paramêtre*/}
+            <div id="menuContainer" className="menuContainer changeMenuContainerFermeture">
+                <div className="iconContainerSpace"></div>
+                <h3>Menu</h3>
+                <div>Profil</div>
+                <div>Langues</div>
+                <div>Paramètres de confidentialité</div>
+                <label className="switch">
+                    <input type="checkbox"/>
+                    <span className="slider"></span>
+                </label>
+                <div>Mode sombre</div>
+            </div>
 
+            {/* "Ecran" surlequel s'affiche les autres pages */}
+            <iframe id='iframeShownPage' src={urlOrigin + '/' + paramPage} frameBorder="0"></iframe>
+
+            {/* Le pied de page (les boutons pour aller de page en page) */}
             <div className="footer">
 				<button className="footerButton" onClick={()=>{navigate('/globalPage?page=Place')}} ><img src={paramPage=='Place' ? homeWhiteModeChecked : homeWhiteMode}/></button>
 				<button className="footerButton" onClick={()=>{navigate('/globalPage?page=AddCamera')}} ><img src={paramPage=='AddCamera' ? plusWhiteModeChecked : plusWhiteMode}/></button>
@@ -41,5 +63,20 @@ function AddCamera()
 		</div>
 	);
 };
+
+
+function toggleMenuParam(){
+    console.log("aaa")
+    let elementParam = document.getElementById("param")
+    if (elementParam != null){
+        elementParam.classList.toggle("change")
+    }
+
+    let elementMenuContainer = document.getElementById("menuContainer")
+    if (elementMenuContainer != null){
+        elementMenuContainer.classList.toggle("changeMenuContainerOuverture")
+        elementMenuContainer.classList.toggle("changeMenuContainerFermeture")
+    }
+}
 
 export default AddCamera;
--- src/pages/Place.js
+++ src/pages/Place.js
@@ -64,7 +64,6 @@ function Place()
 			<div className="topShadow"></div>
 			<div className="searchBarContainer">
 				<img src={wiveLogoTitre} />
-				<div id="param" className="paramIcon" onClick={() => document.getElementById("param").classList.toggle("change")}><div className="bar1"></div><div className="bar2"></div><div className="bar3"></div></div>
 				<div className="searchBar"><input type="text" placeholder="cherchez un lieu" /></div>
 			</div>
 
