d45e70ff1242abc1c6cb222e04592142edacd10f
Ronpu <ronpu@hotmail.fr>
2024-05-25 17:01:24 +0200

    FavoriteCamera visuel

--- src/pages/ChooseCamera.js
+++ src/pages/ChooseCamera.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/global.css";
-import "../css/ChooseCamera.css";
+import "../css/ChooseCamera.css"
+
 
 // IMPORT DES IMAGES :
 import centreCommercial from "../assets/defaultImages/Centre commercial.jpg";
@@ -10,6 +11,7 @@ import montBlanc from "../assets/defaultImages/Mont Blanc.jpg";
 import parc from "../assets/defaultImages/Parc.jpg";
 import plage from "../assets/defaultImages/Plage.jpg";
 import salleDeSport from "../assets/defaultImages/Salle de sport.jpg";
+import noImageFound from "../assets/noImageFound.png";
 
 function ChooseCamera()
 {
@@ -22,15 +24,24 @@ function ChooseCamera()
                 <div className="titreMoyen">Découvrez vos alentours</div>
                 <ul className="carousel">
                     <li>
-                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-1"/><label for="CB-alentours-1"><img className="favoriteHeart"/></label>
-                        <img className="chooseImage" src={salleDeSport} /><div>Salle de sport</div>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-1"/><label htmlFor="CB-alentours-1"><img className="favoriteHeart"/></label>
+                        <img className="chooseCameraImage" src={salleDeSport} /><div>Salle de sport</div>
                     </li>
                     <li>
-                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-2"/><label for="CB-alentours-2"><img className="favoriteHeart" /></label>
-                        <img className="chooseImage" src={parc} /><div>Parc</div></li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-2"/><label htmlFor="CB-alentours-2"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={parc} /><div>Parc</div>
+                    </li>
                     <li>
-                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-3"/><label for="CB-alentours-3"><img className="favoriteHeart" /></label>
-                        <img className="chooseImage" src={centreCommercial} /><div>Centre commercial</div>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-3"/><label htmlFor="CB-alentours-3"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={centreCommercial} /><div>Centre commercial</div>
+                    </li>
+                    <li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-4"/><label htmlFor="CB-alentours-4"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={noImageFound} /><div>Caméra4</div>
+                    </li>
+                    <li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-alentours-5"/><label htmlFor="CB-alentours-5"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={noImageFound} /><div>Caméra5</div>
                     </li>
                 </ul>
                 <a href="https://google.fr" className="textePetit" style={{float: "right"}}>voir plus</a>
@@ -39,14 +50,24 @@ function ChooseCamera()
                 <div className="titreMoyen">Découvrez les plus beaux paysages</div>
                 <ul className="carousel">
                     <li>
-                        <input type="checkbox" className="favoriteCheckbox" id="CB-paysages-1"/><label for="CB-paysages-1"><img className="favoriteHeart" /></label>
-                        <img className="chooseImage" src={kilimandjaro} /><div>Kilimandjaro</div>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-paysages-1"/><label htmlFor="CB-paysages-1"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={kilimandjaro} /><div>Kilimandjaro</div>
                     </li>
-                    <li><input type="checkbox" className="favoriteCheckbox" id="CB-paysages-2"/><label for="CB-paysages-2"><img className="favoriteHeart" /></label>
-                        <img className="chooseImage" src={montBlanc} /><div>Mont blanc</div>
+                    <li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-paysages-2"/><label htmlFor="CB-paysages-2"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={montBlanc} /><div>Mont Blanc</div>
                     </li>
-                    <li><input type="checkbox" className="favoriteCheckbox" id="CB-paysages-3"/><label for="CB-paysages-3"><img className="favoriteHeart" /></label>
-                        <img className="chooseImage" src={plage} /><div>Plage</div>
+                    <li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-paysages-3"/><label htmlFor="CB-paysages-3"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={plage} /><div>Plage</div>
+                    </li>
+                    <li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-paysages-4"/><label htmlFor="CB-paysages-4"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={noImageFound} /><div>Caméra4</div>
+                    </li>
+                    <li>
+                        <input type="checkbox" className="favoriteCheckbox" id="CB-paysages-5"/><label htmlFor="CB-paysages-5"><img className="favoriteHeart" /></label>
+                        <img className="chooseCameraImage" src={noImageFound} /><div>Caméra5</div>
                     </li>
                 </ul>
                 <a href="https://google.fr" className="textePetit" style={{float: "right"}}>voir plus</a>
--- src/pages/FavoriteCamera.js
+++ src/pages/FavoriteCamera.js
@@ -1,35 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// import "../css/global.css";
+import "../css/global.css";
+import "../css/FavoriteCamera.css"
+
 
 // IMPORT DES IMAGES :
-import wiveCenterWhiteMode from "../assets/whiteMode/wiveCenterWhiteMode.svg";
-import paramWhiteMode from "../assets/whiteMode/paramWhiteMode.svg";
+import centreCommercial from "../assets/defaultImages/Centre commercial.jpg";
+import kilimandjaro from "../assets/defaultImages/Kilimandjaro.jpg";
+import montBlanc from "../assets/defaultImages/Mont Blanc.jpg";
+import parc from "../assets/defaultImages/Parc.jpg";
+import plage from "../assets/defaultImages/Plage.jpg";
+import salleDeSport from "../assets/defaultImages/Salle de sport.jpg";
+import noImageFound from "../assets/noImageFound.png";
 
 function FavoriteCamera()
 {
     const navigate = new useNavigate() 
+   
 	return (
 		<div id="body">
-            <table>
-                <tr>
-                    <td>Wive!</td>
-                    <td><img src={paramWhiteMode} /></td>
-                </tr>
-            </table>
-
-            <div>Caméras favorites</div>
-            <table>
-                <tr>
-                    <td><img src={wiveCenterWhiteMode} /><div>Salle de sport</div></td>
-                    <td><img src={wiveCenterWhiteMode} /><div>Parc</div></td>
-                    <td><img src={wiveCenterWhiteMode} /><div>Centre commercial</div></td>
-                </tr>
-            </table>
-            <br/>
-
+            <div className="iconContainerSpace"></div>
+            
+            <div className="menuBody">
+                <div className="titreMoyen">Caméras favorites</div>
+                <table className="favoriteCameraTable">
+                    <tr>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-1" defaultChecked /><label htmlFor="CB-favorite-1"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={salleDeSport} /><div>Salle de sport</div>
+                        </td>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-2" defaultChecked /><label htmlFor="CB-favorite-2"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={parc} /><div>Parc</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-3" defaultChecked /><label htmlFor="CB-favorite-3"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={centreCommercial} /><div>Centre commercial</div>
+                        </td>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-4" defaultChecked /><label htmlFor="CB-favorite-4"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={kilimandjaro} /><div>Kilimandjaro</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-5" defaultChecked /><label htmlFor="CB-favorite-5"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={montBlanc} /><div>Mont Blanc</div>
+                        </td>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-6" defaultChecked /><label htmlFor="CB-favorite-6"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={plage} /><div>Plage</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-7" defaultChecked /><label htmlFor="CB-favorite-7"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={noImageFound} /><div>Caméra7</div>
+                        </td>
+                        <td>
+                            <input type="checkbox" className="favoriteCheckbox" id="CB-favorite-8" defaultChecked /><label htmlFor="CB-favorite-8"><img className="favoriteHeart" /></label>
+                            <img className="favoriteCameraImage" src={noImageFound} /><div>Caméra8</div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
 		</div>
 	);
 };
+
+// function changeCheckbox(checkboxId){
+//     let element = document.getElementById(checkboxId)
+//     if (element.checked == true) {
+//         element.checked = "false"
+//         console.log("false")
+//     }
+//     else{
+//         element.checked = "true"
+//         console.log("true")
+
+//     }
+// }
 
 export default FavoriteCamera;
